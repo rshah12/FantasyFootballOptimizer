e104261e1bb79a34f54b7ff2f7100b99536cf53e
Raj Patel <patelr1@dhcp-v880-22-03994.1Xwireless.unc.edu>
2017-12-10 16:41:51 -0500

    Added getPlayers

---
+++ getPlayers.php
@@ -0,0 +1,15 @@
+<?php
+  $conn = new mysqli("classroom.cs.unc.edu","patelr1","comp426Group","patelr1db");
+
+  $QBs = array();
+  $Query = "SELECT * FROM ppool WHERE pos = 'QB' ORDER BY CPP";
+  if ($Result=mysqli_query($conn,$Query)){
+    while ($obj=mysqli_fetch_object($Result)){
+      $player = new player($obj->fname, $obj->lname, $obj->pos, $obj->team, $obj->opponent, $obj->salary, $obj->cpp, $obj->projection);
+      array_push($QBs, $player);
+    }
+  mysqli_free_result($Result);
+  }
+
+  echo sizof($QBs);
+?>
---
+++ playerTable.js
@@ -0,0 +1,3 @@
+var playerTable = function(){
+  
+}
