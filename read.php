6466d2e78fbc3c46fa2b4243290b18017c773b10
bradericmurphy <bradleyericmurphy@gmail.com>
2017-12-10 15:32:37 -0500

    upload is now functioning

--- read.php
+++ read.php
@@ -1,5 +1,32 @@
 <?php
-  echo 'Current PHP version: ' . phpversion();
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($fileType != "txt") {
+    echo "Sorry, only .txt files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+  }
+  ///
   date_default_timezone_set('America/New_York');
   $conn = new mysqli("classroom.cs.unc.edu","patelr1","comp426Group","patelr1db");
 
@@ -27,8 +54,7 @@
   );";
   $conn->query($Query);
 
-
-  $file = fopen("projections.txt","r");
+  $file = fopen($target_file,"r");
 
   while(!feof($file)){
     $line = fgets($file);
@@ -55,4 +81,4 @@
 
   $conn->close();
   fclose($file);
-?>
+  ?>
