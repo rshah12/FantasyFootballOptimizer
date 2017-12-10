7db216074f19ef8644dc3073e4f3134dea01e4d8
Raj Patel <patelr1@dhcp-v880-22-03994.1Xwireless.unc.edu>
2017-12-10 17:09:24 -0500

    getting on the same page

--- getPlayers.php
+++ getPlayers.php
@@ -1,8 +1,9 @@
+<?php include("player.php"); ?>
 <?php
   $conn = new mysqli("classroom.cs.unc.edu","patelr1","comp426Group","patelr1db");
 
   $QBs = array();
-  $Query = "SELECT * FROM ppool WHERE pos = 'QB' ORDER BY CPP";
+  $Query = "SELECT * FROM ppool WHERE pos = 'QB' ORDER BY cpp";
   if ($Result=mysqli_query($conn,$Query)){
     while ($obj=mysqli_fetch_object($Result)){
       $player = new player($obj->fname, $obj->lname, $obj->pos, $obj->team, $obj->opponent, $obj->salary, $obj->cpp, $obj->projection);
@@ -11,5 +12,55 @@
   mysqli_free_result($Result);
   }
 
-  echo sizof($QBs);
+  $RBs = array();
+  $Query = "SELECT * FROM ppool WHERE pos = 'RB' ORDER BY cpp";
+  if ($Result=mysqli_query($conn,$Query)){
+    while ($obj=mysqli_fetch_object($Result)){
+      $player = new player($obj->fname, $obj->lname, $obj->pos, $obj->team, $obj->opponent, $obj->salary, $obj->cpp, $obj->projection);
+      array_push($RBs, $player);
+    }
+  mysqli_free_result($Result);
+  }
+
+  $WRs = array();
+  $Query = "SELECT * FROM ppool WHERE pos = 'WR' ORDER BY cpp";
+  if ($Result=mysqli_query($conn,$Query)){
+    while ($obj=mysqli_fetch_object($Result)){
+      $player = new player($obj->fname, $obj->lname, $obj->pos, $obj->team, $obj->opponent, $obj->salary, $obj->cpp, $obj->projection);
+      array_push($WRs, $player);
+    }
+  mysqli_free_result($Result);
+  }
+
+  $TEs = array();
+  $Query = "SELECT * FROM ppool WHERE pos = 'TE' ORDER BY cpp";
+  if ($Result=mysqli_query($conn,$Query)){
+    while ($obj=mysqli_fetch_object($Result)){
+      $player = new player($obj->fname, $obj->lname, $obj->pos, $obj->team, $obj->opponent, $obj->salary, $obj->cpp, $obj->projection);
+      array_push($TEs, $player);
+    }
+  mysqli_free_result($Result);
+  }
+
+  $Ds = array();
+  $Query = "SELECT * FROM ppool WHERE pos = 'D' ORDER BY cpp";
+  if ($Result=mysqli_query($conn,$Query)){
+    while ($obj=mysqli_fetch_object($Result)){
+      $player = new player($obj->fname, $obj->lname, $obj->pos, $obj->team, $obj->opponent, $obj->salary, $obj->cpp, $obj->projection);
+      array_push($Ds, $player);
+    }
+  mysqli_free_result($Result);
+  }
+
+  $Ks = array();
+  $Query = "SELECT * FROM ppool WHERE pos = 'K' ORDER BY cpp";
+  if ($Result=mysqli_query($conn,$Query)){
+    while ($obj=mysqli_fetch_object($Result)){
+      $player = new player($obj->fname, $obj->lname, $obj->pos, $obj->team, $obj->opponent, $obj->salary, $obj->cpp, $obj->projection);
+      array_push($Ks, $player);
+    }
+  mysqli_free_result($Result);
+  }
+
+  echo $QBs[0];
 ?>
