43d512136ab3f065be2c4bc37d9135d32717e186
Raj Patel <patelr1@Rajs-MacBook.local>
2017-12-11 15:09:48 -0500

    I think this works

--- getPlayers.php
+++ getPlayers.php
@@ -64,7 +64,7 @@
   }
 
   $Lineups = array();
-  $maxLineups = 2000;
+  $maxLineups = 100;
   $maxExposure = 50;
   $requestedLineups = 100;
   $minProjection = 0;
@@ -73,30 +73,38 @@
 
     for($i=0; $i<7; $i++){
         $QB = $QBs[$i];
-        //if(sizeof($Lineups) >= $maxLineups){break;}
+        if(sizeof($Lineups) >= $maxLineups){break;}
       for($j=0; $j<10; $j++){
           $RB = $RBs[$j];
-          //if(sizeof($Lineups) >= $maxLineups){break;}
+          if(sizeof($Lineups) >= $maxLineups){break;}
         for($k=0; $k<10; $k++){
             $RB2 = $RBs[$k+$j];
+            if(sizeof($Lineups) >= $maxLineups){break;}
             if($RB2 != $RB){
           for($l=0; $l<15; $l++){
               $WR = $WRs[$l];
+              if(sizeof($Lineups) >= $maxLineups){break;}
               //if($WR->sal+$RB2->sal+$RB->sal+$QB->sal+22000 > 60000){break;}
             for($m=0; $m<15; $m++){
                 $WR2 = $WRs[$m+$l];
+                if(sizeof($Lineups) >= $maxLineups){break;}
                 if($WR2 != $WR){
                 //if($WR2->sal+$WR->sal+$RB2->sal+$RB->sal+$QB->sal+17500 > 60000){break;}
               for($n=0; $n<15; $n++){
                   $WR3 = $WRs[$n+$m+$l];
+                  if(sizeof($Lineups) >= $maxLineups){break;}
                   if($WR3 != $WR2 && $WR3 != $WR){
                   //if($WR3->sal+$WR2->sal+$WR->sal+$RB2->sal+$RB->sal+$QB->sal+13000 > 60000){break;}
                 for($o=0; $o<5; $o++){
                     $TE = $TEs[$o];
+                    if(sizeof($Lineups) >= $maxLineups){break;}
                     //if($TE->sal+$WR3->sal+$WR2->sal+$WR->sal+$RB2->sal+$RB->sal+$QB->sal+8500 > 60000){break;}
                   for($p=0; $p<5; $p++){
                       $K = $Ks[$p];
+                      if(sizeof($Lineups) >= $maxLineups){break;}
                     for($q=0; $q<5; $q++){
+                      $D = $Ds[$q];
+                      if(sizeof($Lineups) >= $maxLineups){break;}
                       $lineup = new lineup($QB, $RB, $RB2, $WR, $WR2, $WR3, $TE, $K, $D);
                       //echo "$lineup->salary\n";
                       if ($lineup->salary > 57000 && $lineup->salary <= 60000){
@@ -109,9 +117,8 @@
                           }
                         } else if ($lineup->projection > $minProjection){
                             echo "Added Lineup Projection: "."$lineup->projection"." at Lineup#"."$counter\n";
-                            //echo $lineup->$QB->$fname;
-                            //echo "$lineup->$QB->$lname"." "."$lineup->$RB->$lname"." "."$lineup->$RB2->$lname"." "."$lineup->$WR->$lname"." "."$lineup->$WR2->$lname\n";
-                            //echo "$lineup->$WR3->$lname"." "."$lineup->$TE->$lname"." "."$lineup->$K->$lname"." "."$lineup->$D->$lname\n";
+                            echo "$QB->lname"." "."$RB->lname"." "."$RB2->lname"." "."$WR->lname"." "."$WR2->lname\n";
+                            echo "$WR3->lname"." "."$TE->lname"." "."$K->lname"." "."$D->lname\n";
                             $Lineups[$minIndex] = $lineup;
                             $minProjection = 1000;
                             for($z=0; $z<count($Lineups);$z++){
@@ -135,5 +142,20 @@
         }
       }
       }
+    }
+
+    foreach($Lineups as $l){
+      echo "Lineup Projection: "."$l->projection\n";
+      $QB = $l->QB;
+      $RB = $l->RB;
+      $RB2 = $l->RB2;
+      $WR = $l->WR;
+      $WR2 = $l->WR2;
+      $WR3 = $l->WR3;
+      $TE = $l->TE;
+      $K = $l->K;
+      $D = $l->D;
+      echo "Players "."$QB->lname"." "."$RB->lname"." "."$RB2->lname"." "."$WR->lname"." "."$WR2->lname\n";
+      echo "$WR3->lname"." "."$TE->lname"." "."$K->lname"." "."$D->lname\n";
     }
 ?>
