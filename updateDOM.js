5820332b3d65909237ff96034db6e8f5e8875a33
rshah12 <Richa_Shah@kenan-flagler.unc.edu>
2017-12-14 18:45:22 -0500

    lrkaj

--- updateDOM.js
+++ updateDOM.js
@@ -1,6 +1,40 @@
-$(function () {
+$(document).ready(function () {
+
+            fillData = function () {
+
+                $.ajax({
+                    method: "GET",
+                    url: "getPlayers.php",
+
+                }).done(function (data) {
+
+                        var result = $.parseJSON(data);
+
+                        var string = '<table><tr><th>#</th><th>Name</th><th>Email</th></tr>';
+
+                        //from result create a string of data and append to the div
+                        $.each(result, function (key, value) {
+
+                            string += "<tr>"
+                            "<td>" + value['fname'] + "</td>"
+                            "<td>" + value['lname'] + "</td>"
+                            "<td>" + value['pos'] + "</td>"
+                            "<td>" + value['team'] + "</td>"
+                            "<td>" + value['opp'] + "</td>"
+                            "<td>" + value['sal'] + "</td>"
+                            "<td>" + value['cpp'] + "</td>"
+                            "<td>" + value['projection']+"</td>"
+                            "<td>" + value['uses']+"</td>"
+                            "</tr>";
+
+                        });
+
+                        string += '</table>';
+
+                        $("#records").html(string);
 
 
 
+                    };
 
-});
+                });
